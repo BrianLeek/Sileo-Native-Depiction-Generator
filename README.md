0eaab3ca48ec7b5b80f49ec27d99b412df0d8620
Brian <brianleek2016@gmail.com>
2019-06-19 15:39:04 -0600

    0.4 Update
    Changelog:
     - Added changelog support.
     - Added tint color support.
     - Edited template files.
     - Fixed bugs.

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 Sileo Native Depiction Generator is used to create native depiction files to use for the Sileo application on your repo. The script uses a JSON file with placeholder text inside and when the script runs it creates a copy of that file and replaces the placeholder text with information that the users enters. I created this little project to help developers quickly create depiction files for their packages and to hopefully save them sometime.
 
 # Changelog
+## 0.4:
+ - Added changelog support.
+ - Added tint color support.
+ - Edited template files.
+ - Fixed bugs.
 ## 0.3:
  - Removed some error handling as it was throwing false positives.
  - Added full support for screenshots.
@@ -17,14 +22,12 @@ Sileo Native Depiction Generator is used to create native depiction files to use
 
 # To Do
  - Find away to make it easier for users to use their own templates.
- - Add changelog support.
  - Add more templates.
- - Add tint color support.
  - Type up some documentation.
   
 Thanks to everybody who has helped!
 
-Mathsh - Contributed Code (https://www.reddit.com/user/Mathsh) - Helped with adding screenshot support.  
+Mathsh - Contributed Code (https://www.reddit.com/user/Mathsh) - Helped with adding screenshot support and changelog support.  
 Jon Betts - Contributed Code (https://stackoverflow.com/users/3569627/jon-betts) - Helped with some code and a question I had.  
   
 If you would like to help with this project in anyway please feel free any work you do you will be credited for it.
--- generator.py
+++ generator.py
@@ -11,13 +11,28 @@ import config
 #
 # Brian Leek - Developer (https://brianleek.me/)
 # Mathsh - Contributed Code (https://www.reddit.com/user/Mathsh)
-#   - Helped with adding screenshot support and answered some questions I had.
+#   - Helped with adding screenshot support.
+#   - Helped with adding changelog support.
+#   - Helped fixed some bugs.
+#   - Helped with some questions I had.
 # Jon Betts - Contributed Code (https://stackoverflow.com/users/3569627/jon-betts)
 #   - Helped with some code and a question I had.
 ####################
 
 # Put the questions to ask the user in a function to use later.
 # By adding them into a function it makes it might make it easier when adding a templale to the script.
+def TintColorQuestion():
+    global tintcolor
+    tintcolor = input("Tint Color (default: #2cb1be): ")
+    while True:
+        try:
+            if tintcolor == "":
+                tintcolor = "#2cb1be"
+                continue
+            else:
+                break
+        except ValueError:
+            break
 def HeaderImageQuestion():
     global headerimage
     headerimage = input("Header Image URL: ")
@@ -149,37 +164,37 @@ def SupportURLQuestion():
     global supporturl
     supporturl = input("Support URL: ")
 
-print("This script will generate/create you a native depiction file for Sileo but some information like changelog/version history will not be include because support for it has not yet been added so you will have to edit the depiction file after to add that information. This script will be able to add changelog/version history in the future just not yet.")
+print("This script will ask you some questions about your package and generate you a native depiction file with the info your enter.")
 print("You may leave some fields blank.")
 print("Pick Template")
-# List all files in the templates folder.
+# List all files in the templates folder that end in .json.
 templatepath = config.TemplatesPath
-files = os.listdir(templatepath)
-for name in files:
-    print(name)
+for templates in os.listdir(templatepath):
+    if templates.endswith(".json"):
+        print(os.path.join(templates))
 template = input("Template Number: ")
 
 # Start Template 1
 if template == "1":
-    # This is the template file that is going to be used to create your depiction file.
+    # The name of the template that is being used.
     TemplateFileName = "template1.json"
+    DefaultTintColor = "#2cb1be"
 
     try:
         # Call the questions that are in the functions above to ask the user some information to add to the depiction file later.
+        TintColorQuestion()
         PackageIDQuestion()
         HeaderImageQuestion()
         PackageNameQuestion()
         PackageDescripionQuestion()
-        FirstVersionQuestion()
-        FirstReleaseDateQuestion()
         LatestVersionQuestion()
         LatestReleasedDateQuestion()
         PriceQuestion()
         DeveloperQuestion()
         SupportURLQuestion()
 
-        # Get the placeholder text in the template file and tells the script to replace the information the user enters.
-        replacements = {'Header Image URL':headerimage, 'Package Name':packagename, 'Package Description':description, 'First Released Version Number':firstversion, 'First Release Date':firstreleasedate, 'Latest Version Number':latestversion, 'Latest Release Date':latestreleasedate, 'Package Price':price, 'Package Developer':developer, 'Support URL':supporturl}
+        # Get the placeholder text in the template file and tells the script to replace that with what the user inputs.
+        replacements = {'Tint Color':tintcolor, 'Header Image URL':headerimage, 'Package Name':packagename, 'Package Description':description, 'Latest Version Number':latestversion, 'Latest Release Date':latestreleasedate, 'Package Price':price, 'Package Developer':developer, 'Support URL':supporturl}
         # Remove placeholder text if input field is left blank.
         removeplaceholdertext = {'Header Image URL':'', 'Support URL':''}
 
@@ -190,9 +205,9 @@ if template == "1":
         except ValueError:
             print("Failed to create the folder to save your depiction file. Please check config.py.")
 
-        # Try and create the depiction file with the name entered in the package id question.
+        # Try and create the depiction file with the input entered in the package id question.
         with open(config.TemplatesPath+TemplateFileName) as infile, open(config.SaveDepictionPath+packageid, 'w') as outfile:
-            # Replace the placeholder text that was entered in "replacements" with the information the user entered.
+            # Replace the placeholder text that was entered in "replacements" with the information the user inputs.
             try:
                 for createdepictionfile in infile:
                     for src, target in replacements.items():
@@ -217,45 +232,71 @@ if template == "1":
         with open(file_name) as fh:
             full_data = json.load(fh)
 
-        # Thanks Mathsh for the code! (https://www.reddit.com/user/Mathsh)
         screenshot_template = full_data['tabs'][0]['views'][1]['screenshots'][0]
         full_data['tabs'][0]['views'][1]['screenshots'].pop(0)
-        screenshot_url = input("Please enter a screenshot url.  Enter a blank string when you are finished: ")
+        screenshot_url = input("Screenshot URL: ")
         while screenshot_url != "":
             modified_screenshot_template = screenshot_template
             modified_screenshot_template['url'] = screenshot_url
             modified_screenshot_template['fullSizeURL'] = screenshot_url
             full_data['tabs'][0]['views'][1]['screenshots'].append(modified_screenshot_template.copy())
-            screenshot_url = input("Please enter a screenshot url.  Enter a blank string when you are finished: ")
+            screenshot_url = input("Screenshot URL (Enter a blank string when you are finished): ")
+        if full_data['tabs'][0]['views'][1]['screenshots'] == []:
+            full_data['tabs'][0]['views'].pop(1)
+
+        # Thanks Mathsh for the code! (https://www.reddit.com/user/Mathsh)
+        changelog_header_template = full_data['tabs'][1]['views'].pop(0)
+        changelog_body_template = full_data['tabs'][1]['views'].pop(0)
+        version_number = input('Version Number (Newest to oldest): ')
+        while version_number != '':
+            print("Please enter one change at a time once you entered some info you can hit return/enter to input another change or don't enter anything and just press return/enter when finished.")
+            changes = ''
+            change = input(version_number + ' Changes (Enter a blank string when you are finished): ')
+            while change != '':
+                changes = changes + '\t\n\u2022 ' + change + ' '
+                change = input(version_number + ' Changes (Enter a blank string when you are finished): ')
+            changes = changes + ''
+            modified_changelog_header_template = changelog_header_template
+            modified_changelog_header_template['title'] = version_number
+            modified_changelog_body_template = changelog_body_template
+            modified_changelog_body_template['markdown'] = changes
+            full_data['tabs'][1]['views'].append(changelog_header_template.copy())
+            full_data['tabs'][1]['views'].append(changelog_body_template.copy())
+            version_number = input('Version Number (Newest to oldest) (Enter a blank string when you are finished): ')
+        if full_data['tabs'][1]['views'] == []:
+            full_data['tabs'].pop(1)
 
         # Save the data
         with open(file_name, 'w') as fh:
-            json.dump(full_data, fh, indent=3)
+            json.dump(full_data, fh, indent=4)
 
         print("Successfully generated your depiction file.")
     except ValueError:
         print("Failed to generate your depiction file.")
-# End Template 1
 
+    # Don't close the window until the user wants to.
+    input("Press enter to exit.")
+# End Template 1
 
 # Start Template 2
 elif template == "2":
     # The name of the template that is being used.
     TemplateFileName = "template2.json"
+    DefaultTintColor = "#2cb1be"
 
     try:
         # Call the questions that are in the functions above to ask the user some information to add to the depiction file later.
+        TintColorQuestion()
         PackageIDQuestion()
         HeaderImageQuestion()
         PackageDescripionQuestion()
-        FirstVersionQuestion()
         LatestVersionQuestion()
         PriceQuestion()
         DeveloperQuestion()
         SupportURLQuestion()
 
-        # Get the placeholder text in the template file and tells the script to replace the information the user enters.
-        replacements = {'Header Image URL':headerimage, 'Package Description':description, 'First Released Version Number':firstversion, 'Latest Version Number':latestversion, 'Package Price':price, 'Package Developer':developer, 'Support URL':supporturl}
+        # Get the placeholder text in the template file and tells the script to replace that with what the user inputs.
+        replacements = {'Tint Color':tintcolor, 'Header Image URL':headerimage, 'Package Description':description, 'Latest Version Number':latestversion, 'Package Price':price, 'Package Developer':developer, 'Support URL':supporturl}
         # Remove placeholder text if input field is left blank.
         removeplaceholdertext = {'Header Image URL':'', 'Support URL':''}
 
@@ -266,9 +307,9 @@ elif template == "2":
         except ValueError:
             print("Failed to create the folder to save your depiction file. Please check config.py.")
 
-        # Try and create the depiction file with the name entered in the package id question.
+        # Try and create the depiction file with the input entered in the package id question.
         with open(config.TemplatesPath+TemplateFileName) as infile, open(config.SaveDepictionPath+packageid, 'w') as outfile:
-            # Replace the placeholder text that was entered in "replacements" with the information the user entered.
+            # Replace the placeholder text that was entered in "replacements" with the information the user inputs.
             try:
                 for createdepictionfile in infile:
                     for src, target in replacements.items():
@@ -296,15 +337,37 @@ elif template == "2":
         # Thanks Mathsh for the code! (https://www.reddit.com/user/Mathsh)
         screenshot_template = full_data['tabs'][0]['views'][3]['screenshots'][0]
         full_data['tabs'][0]['views'][3]['screenshots'].pop(0)
-        screenshot_url = input("Please enter a screenshot url.  Enter a blank string when you are finished: ")
+        screenshot_url = input("Screenshot URL: ")
         while screenshot_url != "":
             modified_screenshot_template = screenshot_template
             modified_screenshot_template['url'] = screenshot_url
             modified_screenshot_template['fullSizeURL'] = screenshot_url
             full_data['tabs'][0]['views'][3]['screenshots'].append(modified_screenshot_template.copy())
-            screenshot_url = input("Please enter a screenshot url.  Enter a blank string when you are finished: ")
+            screenshot_url = input("Screenshot URL (enter a blank string when you are finished): ")
         if full_data['tabs'][0]['views'][3]['screenshots'] == []:
             full_data['tabs'][0]['views'].pop(3)
+
+        # Thanks Mathsh for the code! (https://www.reddit.com/user/Mathsh)
+        changelog_header_template = full_data['tabs'][1]['views'].pop(0)
+        changelog_body_template = full_data['tabs'][1]['views'].pop(0)
+        version_number = input('Version Number (Newest to oldest): ')
+        while version_number != '':
+            print("Please enter one change at a time once you entered some info you can hit return/enter to input another change or don't enter anything and just press return/enter when finished.")
+            changes = '<ul>'
+            change = input(version_number + ' Changes: ')
+            while change != '':
+                changes = changes + '\n<li>' + change + '<\/li>'
+                change = input(version_number + ' Changes: ')
+            changes = changes + '<\/ul>'
+            modified_changelog_header_template = changelog_header_template
+            modified_changelog_header_template['title'] = version_number
+            modified_changelog_body_template = changelog_body_template
+            modified_changelog_body_template['markdown'] = changes
+            full_data['tabs'][1]['views'].append(changelog_header_template.copy())
+            full_data['tabs'][1]['views'].append(changelog_body_template.copy())
+            version_number = input('Version Number (Newest to oldest) (Enter a blank string when you are finished): ')
+        if full_data['tabs'][1]['views'] == []:
+            full_data['tabs'].pop(1)
 
         # Save the data
         with open(file_name, 'w') as fh:
@@ -313,8 +376,13 @@ elif template == "2":
         print("Successfully generated your depiction file.")
     except ValueError:
         print("Failed to generate your depiction file.")
+
+    # Don't close the window until the user wants to.
+    input("Press enter to exit.")
 # End Template 2
 
 # Show the user a error if they enter a number for a template that can't be found.
 else:
     print("The template you are looking for can not be found!")
+    # Don't close the window until the user wants to.
+    input("Press enter to exit.")
